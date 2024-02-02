0f7c61395a3e943c133c4d031f820ebea3f21930
Ayomide11705 <99465847+Ayomide11705@users.noreply.github.com>
2024-02-02 03:21:37 +0100

    update: created an about page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <div className='px-2 smallest:px-4 mobile:px-8 laptop:px-20'>
-      <div className='h-full w-full desktop:pt-60 small:pt-[15rem] pt-[10rem]'>
-        <h1 className='tablet:text-4xl tablet:leading-relaxed desktop:px-40 small:text-2xl text-lg leading-10 text-white font-semibold text-center rounded-md flex items-center justify-center py-10'>
+    <div className='h-full w-full px-2 smallest:px-2 laptop:px-20'>
+      <div className='h-full w-full'>
+        <h1 className='w-full h-full tablet:text-4xl tablet:leading-relaxed desktop:px-40 small:text-2xl text-lg leading-10 text-white font-semibold text-center rounded-md flex items-center justify-center'>
           Providing value in properties, simplifying solutions, and building long-term business relationships.
         </h1>
       </div>
